35ce29a1a3950ea1487cc84b27713be9a57e6e11
macmade <macmade>
2008-09-10 05:19:21 +0000

    Added the module script file.

---
+++ helloworld/helloworld.js
@@ -0,0 +1,31 @@
+// <![CDATA[
+    
+/**
+ * JavaScript class for the Drupal 'helloworld' module
+ * 
+ * @author          Jean-David Gadina <macmade@eosgarden.com>
+ * @copyright       Copyright &copy; 2008
+ * @version         0.1
+ */
+function helloworld()
+{
+    /**
+     * Makes a div appear using Scriptaculous
+     * 
+     * @param   id      The ID of the div to display
+     * @return  void
+     */
+    this.display = function( id )
+    {
+        // Gets the requested div
+        var infos = $( id );
+        
+        // Makes it appear
+        Effect.BlindDown( infos );
+    }
+}
+
+// Creates a new instance of the module class
+helloworld = new helloworld();
+
+// ]]>
