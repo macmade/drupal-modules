f8d5644cc34010bc87f146c8ebff41a20047f355
macmade <macmade>
2008-09-10 04:07:04 +0000

    Added inline documentation.

--- oop/classes/Html/Comment.class.php
+++ oop/classes/Html/Comment.class.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * HTML writer class
+ * Class to create HTML comments
  * 
  * @author          Jean-David Gadina <macmade@eosgarden.com>
  * @copyright       Copyright &copy; 2008
@@ -20,29 +20,43 @@ class Html_Comment extends Html_Tag implements ArrayAccess
     const PHP_COMPATIBLE = '5.2.0';
     
     /**
-     * 
+     * The text of the comment
      */
     protected $_comment = '';
     
     /**
+     * Class constructor
      * 
+     * @return NULL
+     * @see     Html_Tag::__construct
      */
     public function __construct( $text )
     {
+        // Sets the comment text
         $this->_comment = $text;
+        
+        // Calls the parent constructor
+        parent::__construct();
     }
     
     /**
+     * Returns the HTML comment
      * 
+     * @param   boolean Wheter the output must be XML compliant
+     * @param   int     THe indentation level
+     * @return  string  The HTML comment, if $xmlCompliant is false, otherwise a blank string
      */
     protected function _output( $xmlCompliant = false, $level = 0 )
     {
+        // Checks if the output must be XML compliant
         if( !$xmlCompliant ) {
             
+            // Returns the HTML comment
             $indent = str_pad( '', $level, self::$_TAB );
             return self::$_NL . $indent . '<!-- ' . $this->_comment . ' -->' . self::$_NL . $indent;
         }
         
+        // Do not return the HTML comment when the output must be XML compliant
         return '';
     }
 }
