1767039d0342cba834323d61caf791aadc9e762c
macmade <macmade>
2008-09-11 06:13:29 +0000

    Added modules to demonstrate the override mechanism.

---
+++ bar/bar.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Override of the Foo module for Drupal
+ * 
+ * @author          Jean-David Gadina <macmade@eosgarden.com>
+ * @copyright       Copyright &copy; 2008
+ * @version         0.1
+ */
+class bar extends foo
+{
+    /**
+     * Class version constants.
+     * Holds the version, the developpment state
+     * and the PHP lower compatible version.
+     */
+    const CLASS_VERSION  = '0.1';
+    const DEVEL_STATE    = 'alpha';
+    const PHP_COMPATIBLE = '5.2.0';
+}
---
+++ foo/foo.class.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Foo module for Drupal
+ * 
+ * @author          Jean-David Gadina <macmade@eosgarden.com>
+ * @copyright       Copyright &copy; 2008
+ * @version         0.1
+ */
+class foo extends Oop_Drupal_ModuleBase
+{
+    /**
+     * Class version constants.
+     * Holds the version, the developpment state
+     * and the PHP lower compatible version.
+     */
+    const CLASS_VERSION  = '0.1';
+    const DEVEL_STATE    = 'alpha';
+    const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * Gets the 'view' section of the module
+     * 
+     * @param   Oop_Xhtml_Tag   The placeholder for the module content
+     * @param   int             The delta offset, used to generate different contents for different blocks
+     * @return  NULL
+     */
+    protected function _getView( Oop_Xhtml_Tag $content, $delta )
+    {
+        // Adds the hello world message
+        $content->div = $this->_lang->hello;
+    }
+}
