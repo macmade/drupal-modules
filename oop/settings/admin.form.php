e0f749f5b49b500019bfab4ec0544f415dbd7ee2
macmade <macmade>
2008-09-11 22:21:37 +0000

    Added the settings form

---
+++ oop/settings/admin.form.php
@@ -0,0 +1,11 @@
+<?php
+
+$formConf = array(
+    'email_crypt_symbol' => array(
+        '#type'          => 'textfield',
+        '#default_value' => '(at)',
+        '#size'          => 5,
+        '#maxlength'     => 5,
+        '#required'      => true,
+    )
+);
