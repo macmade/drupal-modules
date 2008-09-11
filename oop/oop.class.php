0a368ffe9314087c0ccef484c4e52280e97428eb
macmade <macmade>
2008-09-11 22:21:59 +0000

    Added the class for the 'oop' module.

---
+++ oop/oop.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * OOP Framework module for Drupal
+ * 
+ * @author          Jean-David Gadina <macmade@eosgarden.com>
+ * @copyright       Copyright &copy; 2008
+ * @version         0.1
+ */
+class oop extends Oop_Drupal_ModuleBase
+{
+    /**
+     * Class version constants.
+     * Holds the version, the developpment state
+     * and the PHP lower compatible version.
+     */
+    const CLASS_VERSION  = '0.1';
+    const DEVEL_STATE    = 'alpha';
+    const PHP_COMPATIBLE = '5.2.0';
+}
