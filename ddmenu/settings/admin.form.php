c978322728084d8c4250c86fdc80b67737f56e67
macmade <macmade>
2008-09-12 20:03:51 +0000

    Added the administration form.

---
+++ ddmenu/settings/admin.form.php
@@ -0,0 +1,11 @@
+<?php
+
+$formConf = array(
+    'number_of_blocks' => array(
+        '#type'          => 'textfield',
+        '#default_value' => '1',
+        '#size'          => 5,
+        '#maxlength'     => 5,
+        '#required'      => true,
+    )
+);
