82f6a4cf1a0e695356b28a35debee5b13118e6db
macmade <macmade>
2008-09-20 18:57:07 +0000

    Initial import of the 'googlesitemap' module.

---
+++ googlesitemap/settings/admin.form.php
@@ -0,0 +1,11 @@
+<?php
+
+# $Id$
+
+$formConf = array(
+    'linktype' => array(
+        '#type'          => 'select',
+        '#options'       => 'primary,secondary,both',
+        '#default_value' => 'both'
+    )
+);
