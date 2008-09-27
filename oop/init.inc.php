0ff61c6a7bb29d876cfc087dd8c7772bcd269e5a
macmade <macmade>
2008-09-27 08:26:04 +0000

    The OOP initialization is now in a separate file.

---
+++ oop/init.inc.php
@@ -0,0 +1,34 @@
+<?php
+
+# $Id$
+
+// Checks the PHP version
+if( ( double )PHP_VERSION < 5 ) {
+    
+    // We are running PHP4
+    print 'PHP version 5 is required to use this script (actual version is ' . PHP_VERSION . ')';
+    exit();
+}
+
+// Checks for the SPL
+if( !function_exists( 'spl_autoload_register' ) ) {
+    
+    // The SPL is unavailable
+    throw new Exception( 'The SPL (Standard PHP Library) is required to use this script' );
+}
+
+// Includes the class manager
+require_once(
+    dirname( __FILE__ )
+  . DIRECTORY_SEPARATOR
+  . 'classes'
+  . DIRECTORY_SEPARATOR
+  . 'Oop'
+  . DIRECTORY_SEPARATOR
+  . 'Core'
+  . DIRECTORY_SEPARATOR
+  . 'ClassManager.class.php'
+);
+
+// Registers an SPL autoload method to use to load the classes form this package
+spl_autoload_register( array( 'Oop_Core_ClassManager', 'autoLoad' ) );
